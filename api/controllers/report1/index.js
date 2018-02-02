eeb5836a014da342c99387b3634adcbe734d9a5e
fjrhfitriani <fajariahfitriani@gmail.com>
2018-02-02 11:36:20 +0700

    1

--- api/controllers/report1/index.js
+++ api/controllers/report1/index.js
@@ -1,3 +1,4 @@
 module.exports = function(req, res) {
     res.view();
+    
 }
--- i6-core/i6-monitoring/monitoring.js
+++ i6-core/i6-monitoring/monitoring.js
@@ -9,7 +9,9 @@ module.exports = {
 
             var data = {
                 hrsg1 : {},
-                hrsg2 : {}
+                hrsg2 : {},
+                report1 :{},
+                report2 : {}
             };
             _.each(i6.dataSource.hrsg1, function(tag){
                 data.hrsg1[tag.name] = tag;
@@ -19,6 +21,14 @@ module.exports = {
                data.hrsg2[tag.name] = tag;
                if(typeof(tag.value)=='number') data.hrsg2[tag.name].value = tag.value.toFixed(2);
             });
+            _.each(i6.dataSource.report1, function(tag){
+                data.report1[tag.name] = tag;
+                if(typeof(tag.value)=='number') data.report1[tag.name].value = tag.value.toFixed(2);
+             });
+             _.each(i6.dataSource.report2, function(tag){
+                data.report2[tag.name] = tag;
+                if(typeof(tag.value)=='number') data.report2[tag.name].value = tag.value.toFixed(2);
+             });
             sails.sockets.blast('realtime_update',data) ;
         }, 1000);
    
